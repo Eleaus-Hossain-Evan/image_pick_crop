7a9cdd6d907f53e9e251381175096ce6cdab1208
Eleaus-Hossain-Evan <eleaus16-364@diu.edu.bdm>
2023-05-17 21:41:14 +0600

    complete........

--- lib/image_helper.dart
+++ lib/image_helper.dart
@@ -5,8 +5,45 @@ class ImageHelper {
   final ImagePicker _imagePicker;
   final ImageCropper _imageCropper;
   ImageHelper({
-    required ImagePicker? imagePicker,
-    required ImageCropper? imageCropper,
+    ImagePicker? imagePicker,
+    ImageCropper? imageCropper,
   })  : _imagePicker = imagePicker ?? ImagePicker(),
         _imageCropper = imageCropper ?? ImageCropper();
+
+  Future<List<XFile>> pickImage({
+    ImageSource source = ImageSource.gallery,
+    int quality = 100,
+    bool multiple = false,
+  }) async {
+    if (multiple) {
+      return await _imagePicker.pickMultiImage(
+        imageQuality: quality,
+      );
+    } else {
+      final file = await _imagePicker.pickImage(
+        source: source,
+        imageQuality: quality,
+      );
+      if (file != null) {
+        return [file];
+      } else {
+        return [];
+      }
+    }
+  }
+
+  Future<CroppedFile?> crop({
+    required String filePath,
+    CropStyle cropStyle = CropStyle.rectangle,
+  }) async {
+    return await _imageCropper.cropImage(
+      sourcePath: filePath,
+      cropStyle: cropStyle,
+      // compressQuality: 100,
+      // uiSettings: [
+      //   IOSUiSettings(),
+      //   AndroidUiSettings(),
+      // ],
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+
+import 'image_helper.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +15,144 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      home: ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Picker & Cropper'),
       ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          vertical: 80,
+          horizontal: 20,
+        ),
+        children: const [
+          ProfileImage(initials: 'AB'),
+          MultipleImage(),
+        ],
+      ),
+    );
+  }
+}
+
+final imageHelper = ImageHelper();
+
+class ProfileImage extends StatefulWidget {
+  const ProfileImage({
+    Key? key,
+    required this.initials,
+  }) : super(key: key);
+
+  final String initials;
+
+  @override
+  State<ProfileImage> createState() => _ProfileImageState();
+}
+
+class _ProfileImageState extends State<ProfileImage> {
+  File? _image;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: CircleAvatar(
+              backgroundColor: Colors.grey[300],
+              radius: 64,
+              foregroundImage: _image != null ? FileImage(_image!) : null,
+              child: Text(
+                widget.initials,
+                style: const TextStyle(
+                  fontSize: 68,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 16),
+        TextButton(
+          onPressed: () async {
+            final files = await imageHelper.pickImage();
+            if (files.isNotEmpty) {
+              final croppedFile = await imageHelper.crop(
+                filePath: files.first.path,
+                cropStyle: CropStyle.circle,
+              );
+              if (croppedFile != null) {
+                setState(() {
+                  _image = File(croppedFile.path);
+                });
+              }
+            }
+          },
+          child: const Text('Select Photo'),
+        ),
+      ],
+    );
+  }
+}
+
+class MultipleImage extends StatefulWidget {
+  const MultipleImage({super.key});
+
+  @override
+  State<MultipleImage> createState() => _MultipleImageState();
+}
+
+class _MultipleImageState extends State<MultipleImage> {
+  List<File> _images = [];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Wrap(
+          spacing: 4,
+          runSpacing: 4,
+          children: List.generate(_images.length, (index) {
+            final e = _images[index];
+            return InkWell(
+              onTap: () async {
+                final croppedFile = await imageHelper.crop(
+                  filePath: e.path,
+                  cropStyle: CropStyle.circle,
+                );
+                if (croppedFile != null) {
+                  setState(() {
+                    _images[index] = File(croppedFile.path);
+                  });
+                }
+              },
+              child: Ink.image(
+                height: 100,
+                width: 100,
+                fit: BoxFit.cover,
+                image: FileImage(e),
+              ),
+            );
+          }),
+        ),
+        const SizedBox(height: 8),
+        TextButton(
+          onPressed: () async {
+            final files = await imageHelper.pickImage(multiple: true);
+
+            setState(() {
+              _images = files.map((e) => File(e.path)).toList();
+            });
+          },
+          child: const Text('Select Multiple Photos'),
+        ),
+      ],
     );
   }
 }
